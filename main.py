64067f7cca4d31a0147eb45d3d39a5afd72a1ddd
Ing Zhen Lee <ingzhen2003@gmail.com>
2022-08-26 17:17:07 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+friends = ["Jim" , "Karen" , "Kevin"]
+
+for name in friends:
+    print(name)
+
+for index in range(3,6):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+
+    
